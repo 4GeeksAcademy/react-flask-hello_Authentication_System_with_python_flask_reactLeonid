52b674b8d595e77bfb005c900764a3bae8a59449
Leonid ocampo <115747563+leonidocampo-co@users.noreply.github.com>
2023-05-20 02:36:21 +0000

    cambios nuevos

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,12 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_bcrypt import Bcrypt
+
 
 api = Blueprint('api', __name__)
-
+app = Flask(__name__)
+bcrypt = Bcrypt(api)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -16,3 +19,13 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/signup", methods=["POST"])
+def user_create():
+    data = request.get_json()
+    print(data)
+    new_user = User.query.filter_by(email=data["email"]).first()
+    if(new_user is not None)
+        return jsonify({
+            "msg": "Email registrado"
+        })
